69300a6a6ddd08679504bc2c12d9fafb6cafa788
Umakhihe Arnold <umakhihe.arnold@branchd.ng>
2020-01-17 21:13:50 +0000

    working on DataPlayer now

--- src/toptrumps/DataPlayer.java
+++ src/toptrumps/DataPlayer.java
@@ -1,5 +1,26 @@
 package toptrumps;
 
+/**
+ * 
+ * This class represents a player
+ * 
+ * @author Team TRY-CATCH - Arnold Umakhihe 2445734U Team
+ *
+ */
 class DataPlayer {
-
+	/**
+	 * enum to represents the different types of player
+	 * @author salistechltd
+	 */
+	enum PlayerType {
+		AI, HUMAN
+	}
+	
+	/** DataCard array representing the deck of cards */
+	private DataCard[] cardDeck = new DataCard[7];
+	
+	/** PlayerType enum representing the type of player - human or AI */
+	private PlayerType type;
+	
+	
 }
