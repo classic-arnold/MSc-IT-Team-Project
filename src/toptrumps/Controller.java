0232fcc586b6ddda58cef1fd63a0bbd8b783b434
JiaLiang Song (Leon) <56025832+5ggg@users.noreply.github.com>
2020-01-24 16:42:22 +0000

    game loop

--- src/toptrumps/Controller.java
+++ src/toptrumps/Controller.java
@@ -12,62 +12,85 @@ import java.util.Random;
 public class Controller {
     private DataGame dataGame;
     private ViewCLI viewCli;
-    private DataPlayer humman;
+    private DataPlayer human;
 
     public Controller(DataGame dataGame, ViewCLI viewCli) {
-        this.dataGame = DataGame.getInstance(2);
+        this.dataGame = dataGame;
         this.viewCli = viewCli;
-        humman = dataGame.getPlayers()[0];
+        this.human = dataGame.getHumanPlayer();
     }
 
     public void startGame() {
-        running();
+    	int startChoice = this.viewCli.chooseDisplay();
+    	if(startChoice == 1) {   		
+    		this.viewCli.displayStats();
+    	} else if(startChoice == 2) {
+    		
+    	}
+    	
+    	DataGame game = DataGame.getInstance(numberOfArtificialIntelligencePlayers); // used to start a new game
+		
+    	game.startGame();
+		while(game.getGameState()==DataGame.GameState.RUNNING) {
+			Random r = new Random();
+			game.playRound(DataGame.CATEGORYNAMES[r.nextInt(5)]);
+		}
+		
+		DataPlayer[] p = game.getAllPlayers();
+		
+		System.out.println("\nAll players:");
+		for(DataPlayer pl : p) {
+			System.out.println(pl.getName() + ": " + pl.getScore());
+		}
+		
+		System.out.println("Draw: " + game.getNumberOfDraws());
     }
+    
 
-    public void running() {
-        viewCli.chooseDisplay();
-        while (viewCli.getStartChoice() == 2 || viewCli.getStartChoice() == 1) {
-            if(viewCli.getStartChoice() == 2){
-                dataGame.startGame();
-                while (!dataGame.isGameOver()){
-                    int roundnumber = dataGame.getRoundNumber();
-                    viewCli.displayRound(roundnumber);
-                    //Player Card
-                    if(!humman.getDeck().isEmpty()){
-                        viewCli.displayPlayerCard(humman.getDataCard());
-                        viewCli.displayNumDeckCards(humman.getNumberOfCardsInDeck());
-                    }
-
-                    DataPlayer dataPlayer = dataGame.getLastRoundWinner();
-                    if (dataPlayer == null) {
-                        int n = new Random().nextInt(5);
-                        dataPlayer = dataGame.getPlayers()[n];
-                    }
-                    //Get attributes and determine if randomness is required
-                    int categoryChoice = -1;
-                    if (dataPlayer.isHuman()) {
-                        viewCli.displayCategorySelection();
-                        categoryChoice = viewCli.getCategoryChoice();
-                    } else {
-                        categoryChoice = new Random().nextInt(5)+1;
-                    }
-
-                    //Compare
-                    dataGame.playRound(DataCard.CATEGORYNAMES[categoryChoice-1]);
-
-                    //Output result
-                    if (dataGame.isLastRoundWasDraw()) {
-                        viewCli.displayRoundResult(dataGame.getLastRoundWinner(),roundnumber,dataGame.getLastRoundWinningCards()[0],categoryChoice, dataGame.getNumberOfCardsInCommonPile());
-                    } else {
-                        viewCli.displayRoundResult(dataGame.getLastRoundWinner(),roundnumber,dataGame.getLastRoundWinningCards()[0],categoryChoice, 0);
-                    }
-                }
-                viewCli.displayStats(dataGame);
-            } else if(viewCli.getStartChoice() == 1){
-                viewCli.displayStats(dataGame);
-            }
-            viewCli.chooseDisplay();
-        }
+//    public void running() {
+//        viewCli.chooseDisplay();
+//        while (viewCli.getStartChoice() == 2 || viewCli.getStartChoice() == 1) {
+//            if(viewCli.getStartChoice() == 2){
+//                dataGame.startGame();
+//                while (!dataGame.isGameOver()){
+//                    int roundnumber = dataGame.getRoundNumber();
+//                    viewCli.displayRound(roundnumber);
+//                    //Player Card
+//                    if(!humman.getDeck().isEmpty()){
+//                        viewCli.displayPlayerCard(humman.getDataCard());
+//                        viewCli.displayNumDeckCards(humman.getNumberOfCardsInDeck());
+//                    }
+//
+//                    DataPlayer dataPlayer = dataGame.getLastRoundWinner();
+//                    if (dataPlayer == null) {
+//                        int n = new Random().nextInt(5);
+//                        dataPlayer = dataGame.getPlayers()[n];
+//                    }
+//                    //Get attributes and determine if randomness is required
+//                    int categoryChoice = -1;
+//                    if (dataPlayer.isHuman()) {
+//                        viewCli.displayCategorySelection();
+//                        categoryChoice = viewCli.getCategoryChoice();
+//                    } else {
+//                        categoryChoice = new Random().nextInt(5)+1;
+//                    }
+//
+//                    //Compare
+//                    dataGame.playRound(DataCard.CATEGORYNAMES[categoryChoice-1]);
+//
+//                    //Output result
+//                    if (dataGame.isLastRoundWasDraw()) {
+//                        viewCli.displayRoundResult(dataGame.getLastRoundWinner(),roundnumber,dataGame.getLastRoundWinningCards()[0],categoryChoice, dataGame.getNumberOfCardsInCommonPile());
+//                    } else {
+//                        viewCli.displayRoundResult(dataGame.getLastRoundWinner(),roundnumber,dataGame.getLastRoundWinningCards()[0],categoryChoice, 0);
+//                    }
+//                }
+//                viewCli.displayStats(dataGame);
+//            } else if(viewCli.getStartChoice() == 1){
+//                viewCli.displayStats(dataGame);
+//            }
+//            viewCli.chooseDisplay();
+//        }
         System.out.println("exit");
     }
 
