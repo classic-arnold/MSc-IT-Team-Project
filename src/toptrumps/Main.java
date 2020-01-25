d29b26063237912e0054252dd4f9995349ed88f9
Umakhihe Arnold <umakhihe.arnold@branchd.ng>
2020-01-25 19:13:18 +0000

    CLI version v1

--- src/toptrumps/Main.java
+++ src/toptrumps/Main.java
@@ -1,16 +1,16 @@
-package toptrumps;
-
-import java.util.Random;
-
-public class Main {
-
-	public static void main(String[] args) {
-		while(true) {
-			DataGame model = DataGame.resetAndGetInstance(4);
-			ViewCLI view = new ViewCLI(model); // we need to pass in view here
-			Controller controller = new Controller(model, view);
-			controller.startGame();
-		}
-	}
-
-}
+//package toptrumps;
+//
+//import java.util.Random;
+//
+//public class Main {
+//
+//	public static void main(String[] args) {
+////		while(true) {
+////			DataGame model = DataGame.resetAndGetInstance(4);
+////			ViewCLI view = new ViewCLI(model); // we need to pass in view here
+////			Controller controller = new Controller(model, view);
+////			controller.startGame();
+////		}
+//	}
+//
+//}
