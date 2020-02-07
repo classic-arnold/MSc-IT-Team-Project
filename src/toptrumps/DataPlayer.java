39c948cf088bbe1925b280e5871fad2e25aeae5e
Umakhihe Arnold <umakhihe.arnold@branchd.ng>
2020-02-07 15:23:26 +0000

    some methods public

--- src/toptrumps/DataPlayer.java
+++ src/toptrumps/DataPlayer.java
@@ -148,7 +148,7 @@ public class DataPlayer {
 	 * get player score
 	 * @return int representing the player's score for the game
 	 */
-	int getScore() {
+	public int getScore() {
 		return this.score;
 	}
 
