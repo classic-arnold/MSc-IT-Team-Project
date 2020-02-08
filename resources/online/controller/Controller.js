fba29df1f624b525c5c73897dcf1c168a6f04653
JiaLiang Song (Leon) <56025832+5ggg@users.noreply.github.com>
2020-02-08 05:24:23 +0000

    update

--- resources/online/controller/Controller.js
+++ resources/online/controller/Controller.js
@@ -22,6 +22,195 @@ function initalize() {
 //		selectCategory();
 	playRound();
 //		getStats();
+	
+	
+	// This starts the game
+	function startGame() {
+		return new Promise(resolve=>{
+			let numberOfAIPlayersFromUser = 4; //change this to get from button
+		
+			// First create a CORS request, this is the message we are going to send (a get request in this case)
+			var xhr = createCORSRequest('GET', "http://localhost:7777/toptrumps/game/startGame?numberOfAIPlayers=" + numberOfAIPlayersFromUser); // Request type and URL
+
+			// Message is not sent yet, but we can check that the browser supports CORS
+			if (!xhr) {
+				alert("CORS not supported");
+			}
+
+			// CORS requests are Asynchronous, i.e. we do not wait for a response, instead we define an action
+			// to do when the response arrives
+			xhr.onload = function(e) {
+				var responseText = xhr.response; // the text of the response
+				if(responseText == "0"){
+					resolve();
+				}
+			};
+
+			// We have done everything we need to prepare the CORS request, so send it
+			xhr.send();
+		});
+	}
+	
+	function getRoundNumber(){
+		
+		// First create a CORS request, this is the message we are going to send (a get request in this case)
+		var xhr = createCORSRequest('GET', "http://localhost:7777/toptrumps/game/roundNumber"); // Request type and URL
+
+		// Message is not sent yet, but we can check that the browser supports CORS
+		if (!xhr) {
+			alert("CORS not supported");
+		}
+
+		// CORS requests are Asynchronous, i.e. we do not wait for a response, instead we define an action
+		// to do when the response arrives
+		xhr.onload = function(e) {
+			var responseText = xhr.response; // the text of the response
+			$("#status-message").html("Round Number: " + responseText);
+		};
+
+		// We have done everything we need to prepare the CORS request, so send it
+		xhr.send();	
+	}
+	
+	function shouldHumanSelectCategory(){
+		return new Promise(resolve=>{
+			// First create a CORS request, this is the message we are going to send (a get request in this case)
+			var xhr = createCORSRequest('GET', "http://localhost:7777/toptrumps/game/shouldHumanSelectCategory"); // Request type and URL
+
+			// Message is not sent yet, but we can check that the browser supports CORS
+			if (!xhr) {
+				alert("CORS not supported");
+			}
+
+			// CORS requests are Asynchronous, i.e. we do not wait for a response, instead we define an action
+			// to do when the response arrives
+			xhr.onload = function(e) {
+				var responseText = xhr.response; // the text of the response
+				resolve(responseText);
+			};
+
+			// We have done everything we need to prepare the CORS request, so send it
+			xhr.send();	
+		})
+	}
+	
+	function getCategories(){
+		return new Promise(resolve=>{
+			// First create a CORS request, this is the message we are going to send (a get request in this case)
+			var xhr = createCORSRequest('GET', "http://localhost:7777/toptrumps/game/categoryMenu"); // Request type and URL
+
+			// Message is not sent yet, but we can check that the browser supports CORS
+			if (!xhr) {
+				alert("CORS not supported");
+			}
+
+			// CORS requests are Asynchronous, i.e. we do not wait for a response, instead we define an action
+			// to do when the response arrives
+			xhr.onload = function(e) {
+				var responseText = xhr.response; // the text of the response
+				resolve(responseText);
+			};
+
+			// We have done everything we need to prepare the CORS request, so send it
+			xhr.send();	
+		});
+	}
+	
+	async function selectCategoryForHuman(){
+		// First create a CORS request, this is the message we are going to send (a get request in this case)
+		let categoryList = await getCategories();
+		
+		categoryList = changeStringToArray(categoryList);
+		
+		categorySelectedByHuman = categoryList[0];
+		
+		return categorySelectedByHuman;
+		
+	}
+	
+	async function selectCategoryForAI(){
+		// First create a CORS request, this is the message we are going to send (a get request in this case)
+		let categoryList = await getCategories();
+		
+		categoryList = changeStringToArray(categoryList);
+		
+		categorySelectedByHuman = categoryList[0];
+		
+		return categorySelectedByHuman;
+		
+	}
+	
+	function playRound(){
+		let humanChooseCategory = await shouldHumanSelectCategory();
+		
+		let categories = await getCategories();
+		
+		let categorySelected;
+		
+		if(humanChooseCategory == "true"){
+			categorySelectedByHuman = selectCategory();
+		} else {
+			
+		}
+		
+		// First create a CORS request, this is the message we are going to send (a get request in this case)
+		var xhr = createCORSRequest('GET', "http://localhost:7777/toptrumps/game/roundNumber"); // Request type and URL
+
+		// Message is not sent yet, but we can check that the browser supports CORS
+		if (!xhr) {
+			alert("CORS not supported");
+		}
+
+		// CORS requests are Asynchronous, i.e. we do not wait for a response, instead we define an action
+		// to do when the response arrives
+		xhr.onload = function(e) {
+			var responseText = xhr.response; // the text of the response
+		};
+
+		// We have done everything we need to prepare the CORS request, so send it
+		xhr.send();	
+	}
+
+	// This calls the game REST method from TopTrumpsRESTAPI
+	function getStats() {
+
+		// First create a CORS request, this is the message we are going to send (a get request in this case)
+		var xhr = createCORSRequest('GET', "http://localhost:7777/toptrumps/stats/statistics"); // Request type and URL
+
+		// Message is not sent yet, but we can check that the browser supports CORS
+		if (!xhr) {
+			alert("CORS not supported");
+		}
+
+		// CORS requests are Asynchronous, i.e. we do not wait for a response, instead we define an action
+		// to do when the response arrives 
+		xhr.onload = function(e) {
+			var responseText = xhr.response; // the text of the response
+			responseText = changeStringToArray(responseText);
+		};
+
+		// We have done everything we need to prepare the CORS request, so send it
+		xhr.send();		
+	}
+	
+	function changeStringToArray(string){
+		arr = string.split(",");
+		
+		arr.forEach((arrElem, i)=>{
+			arr[i] = arrElem.replace(/[^A-Za-z0-9]/g, "");
+		});
+		
+		return arr;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 			function createCORSRequest(method, url) {
   				var xhr = new XMLHttpRequest();
