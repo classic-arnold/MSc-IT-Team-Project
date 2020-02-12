518c34dfe626ddad88683858c9d12a9abe22e0b4
Estelle <bokyunglee33@gmail.com>
2020-02-12 13:32:16 +0000

    changed database connection, create sql file, add RESTAPI

--- src/toptrumps/CheckDatabase.java
+++ src/toptrumps/CheckDatabase.java
@@ -19,17 +19,17 @@ public class CheckDatabase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ProgramDatabase postgre=new ProgramDatabase();
-		postgre.selectGameStats();
+//		ProgramDatabase postgre=new ProgramDatabase();
+//		postgre.selectGameStats();
 
 		//check selection. 
 		//What about changing selectGameStats method returns 5 values in array so that model could call it?
 		//Currently, model needs to call every single values by using getters like below:
-		System.out.println("Number of games played overall: "+postgre.getGameCount()
-		+"\nHow many times the computer has won: "+postgre.getAIWon()
-		+"\nHow many times the human has won: "+postgre.getHumanWon()
-		+"\nThe average number of draws: "+postgre.getDraws()
-		+"\nThe largest number of rounds played in a single game: "+postgre.getLargestRound());
+//		System.out.println("Number of games played overall: "+postgre.getGameCount()
+//		+"\nHow many times the computer has won: "+postgre.getAIWon()
+//		+"\nHow many times the human has won: "+postgre.getHumanWon()
+//		+"\nThe average number of draws: "+postgre.getDraws()
+//		+"\nThe largest number of rounds played in a single game: "+postgre.getLargestRound());
 
 
 		Connection c = null;
@@ -38,7 +38,7 @@ public class CheckDatabase {
 
 			Class.forName("org.postgresql.Driver");
 
-			c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/TopTrump","postgres", "qmffldqmffld3");
+			c = DriverManager.getConnection("jdbc:postgresql://52.24.215.108:5432/TryCatch","TryCatch", "TryCatch");
 
 //			c = DriverManager.getConnection("jdbc:postgresql://yacata.dcs.gla.ac.uk:5432/postgres", username, password);
 
