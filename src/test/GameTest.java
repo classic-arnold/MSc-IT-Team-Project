b0c1c250493670b6a0aab60b462cc37217a0e248
Umakhihe Arnold <umakhihe.arnold@branchd.ng>
2020-02-16 20:12:10 +0000

    comments

--- src/test/GameTest.java
+++ src/test/GameTest.java
@@ -34,12 +34,16 @@ public class GameTest{
 	@Test
 	public void testAccurateRoundNumber() {
 
-		//check that the correct round number is displayed after after several rounds are played
+		// check that the correct round number is displayed after after several rounds are played
+		// This is done by iterating a value as the game loops, until the game is over
+		// Then we get the last round of the game and compare it with the value we manually iterated
+		// if the game counted the round properly, the two values should be the same
 
-		int round = 1; //variable created by me to manually compare it with what the game outputs
+		int round = 1; //variable created to manually compare it with what the game outputs
 
-		this.model.startGame();
+		this.model.startGame(); // start a game
 
+		// start game loop and play game
 		while(this.model.getGameState()==DataGame.GameState.RUNNING) {
 			int category;
 
@@ -58,6 +62,8 @@ public class GameTest{
 				round++; //the round is manually incremented so that it can be compared with game round
 			}
 
+			assertEquals(round, this.model.getRound().getRoundNumber());
+
 		}
 
 		assertEquals(round, this.model.getRound().getRoundNumber());
@@ -65,75 +71,85 @@ public class GameTest{
 
 	@Test
 	public void testCorrectTopCard() {
-		//getActivePlayer in dataDame, getDeck in dataplayer to get the player's deck, then get the zeroth element class 
-		//model.getInitialShuffledDeck() does the shuffling
-		//check that the correct top card was returned for several rounds
+		// check that the correct top card was returned for several rounds
 
+		// this works by getting the players top card manually and the playing a round and checking the round cards are the players top card
+
+		// start a game
 		this.model.startGame();
 
+		// get the games active players
 		DataPlayer[] players = this.model.getActivePlayers();
 
+		// this will represent the cards of the players we will use for test
 		ArrayList<DataCard> playerCards = new ArrayList<DataCard>();
 
+		// loop through the players and get the top cards
 		for(int i=0;i<players.length;i++) {
 			playerCards.add(players[i].getDeck().get(0));
 		}
 
+		// play 1 round
 		this.model.playRound(DataGame.CATEGORYNAMES[0]);
+
+		// get the round cards
 		DataCard[] cards = this.model.getRound().getRoundCards();
 
+		// check that the round cards and the top cards of the players are equal
 		for(DataCard card : cards) {
 			assertTrue(playerCards.contains(card));
 		}
-
 	}
 
 	@Test
 	public void testNumOfCardsRemainingForPlayers() {
-		//getActivePlayers in datagame, getDeck of the player in dataPlayer, then check the length of the array returned
-
 		//check that the number of cards remaining for each player after each round is accurate
 
+		// start a game
 		this.model.startGame();
 
+		// get the active players
 		DataPlayer[] players = this.model.getActivePlayers();
 
+		// assert that every player has 8 cards, if there are players, then every player must have 8 cards
 		for(DataPlayer player:players) {
 			assert player.getDeck().size() == 8;
 		}
 
+		// play a round
 		this.model.playRound(DataGame.CATEGORYNAMES[0]);
 
+		// get the winner
 		ArrayList<DataPlayer> winners = this.model.getRound().getRoundWinningPlayers();
 
+		// if game wasnt draw, assert that the winning player now has 12 cards, and no longer has 8 cards
 		if(winners.size()==1) {
 			assert winners.get(0).getDeck().size() == 12;
 			assert winners.get(0).getDeck().size() != 8;
+
+			// assert that other players have 7 cards
 			for(DataPlayer player:players) {
 				if(winners.get(0) != player) {
 					assert player.getDeck().size() == 7;
 				}
 			}
-		} else if(winners.size()>1) {
+		} else if(winners.size()>1) { // if game was draw, assert that common pile must have 5 cards, and all players have 7 cards
 			assert this.model.getCardsInCommonPile().length == 5;
 			for(DataPlayer player:players) {
 				assert player.getDeck().size() == 7;
 			}
 		}
 
+		// the total amount of cards
 		int totalAmountOfCards = 0;
+
 		// this loop sums up all players cards in their decks
 		for(DataPlayer player : players) {
 			totalAmountOfCards += player.getDeck().size();
 		}
 
+		// asert that the sum of all cards in players deck and common pile is 40
 		assert 40 == totalAmountOfCards + this.model.getNumberOfCardsInCommonPile();
-
-		this.model.getRound().incrementRound();
-
-
-		this.model.playRound(DataGame.CATEGORYNAMES[0]);
-
 	}
 
 	@Test
@@ -142,30 +158,41 @@ public class GameTest{
 		//test that the correct winning category is pointed at
 		//test that the correct player was declared winner
 
+		// start the game
 		this.model.startGame();
 
+		// get the unshuffled deck
 		DataCard[] cards = this.model.getInitialUnshuffledDeck();
 
+		// 5 cards
 		DataCard[] card5 = new DataCard[5];
 
+		// get first category
 		String category = DataGame.CATEGORYNAMES[0];
 
+		// get 5 cards from the unshuffled deck, card number 3 has the highest for this category so it should win the round
 		for (int i=0;i<5;i++) {
 			card5[i] = cards[i];
 		}
 
+		// get results from model object from the 5 cards, using this category
 		HashMap<String, Object> map = this.model.getWinningCardsAndPlayers(card5, category);
 
+		// get the winning cards
 		ArrayList<DataCard> winningCards = (ArrayList<DataCard>)map.get("winning cards");
 
+		// the winning cards should contain card number 3, this confirms that the correct card wins
 		assertTrue(winningCards.contains(card5[2]));
 
+		// get all players
 		DataPlayer[] players = this.model.getAllPlayers();
 
+		// remove all cards from players deck
 		for(DataPlayer player : players) {
 			player.removeAllCardsFromDeck();
 		}
 
+		// loop through players and add the 5 cards created to their deck, and put card 3(the winning card) in the 1st player deck
 		for(DataPlayer player : players) {
 			for(DataCard card : card5) {
 				if(player.equals(players[0]) && card.equals(card5[2])) {
@@ -176,25 +203,27 @@ public class GameTest{
 			}
 		}
 
+		// play the round
 		this.model.playRound(category);
 
+		// assert that the 1st player is one of the winning players
 		assertTrue(this.model.getRound().getRoundWinningPlayers().contains(players[0]));
 	}
 
 	@Test
 	public void testGameEnd() {
-		//test that the correct overall winner is returned
-		//create a player, create a deck of just 2 cards, whoever wins that round is supposed to be the winner
-		//test that the winner displayed is correct
-		//test that the correct score for each player is returned
-		//test that the correct score for each player is displayed
+		// test that the correct overall winner is returned
+		// create a player, create a deck of just 2 cards, whoever wins that round is supposed to be the winner
+		// test that the winner displayed is correct
 
+		// create control and check variables to represent round number
 		int lastRoundNumberControl = 0;
 		int lastRoundNumberCheck = 0;
 
+		// start the game
 		this.model.startGame();
 
-
+		// run the game loop
 		while(this.model.getGameState()==DataGame.GameState.RUNNING) {
 			int category;
 
@@ -212,12 +241,16 @@ public class GameTest{
 
 		}
 
+		// store the real last round
 		lastRoundNumberControl = this.model.getRound().getRoundNumber();
 
+		// play one more round
 		this.model.playRound(DataGame.CATEGORYNAMES[0]);
 
+		// store the new round number, if shouldnt increase because the game shouldnt play
 		lastRoundNumberCheck = this.model.getRound().getRoundNumber();
 
+		// if both are equal, this means the game stopped playing when the loop ended
 		assertEquals(lastRoundNumberControl, lastRoundNumberCheck);
 
 
@@ -225,12 +258,15 @@ public class GameTest{
 
 	@Test
 	public void testGameRestart() {
-		//test that when a game ends, the user is able to either start a new game or view statistics
+		// test the game can restart properly
+
+		// stores round number
 		int gameRoundNumber = 0;
 
+		// start a game
 		this.model.startGame();
 
-
+		// run game loop
 		while(this.model.getGameState()==DataGame.GameState.RUNNING) {
 			int category;
 
@@ -247,8 +283,8 @@ public class GameTest{
 
 		}
 
+		// start a new game
 		this.model = new DataGame(4);
-
 		this.model.startGame();
 
 		int category;
@@ -265,8 +301,7 @@ public class GameTest{
 
 		gameRoundNumber = this.model.getRound().getRoundNumber();
 
-		this.model.getRound().incrementRound();
-
+		// if round number is 1, then game restarted properly
 		assertEquals(1, gameRoundNumber);
 
 	}
@@ -278,16 +313,17 @@ public class GameTest{
 		//test that the correct number of AI wins is returned
 		//test that the correct number of draws is returned
 		//test that the largest number of rounds played in a single game is returned
-		//ensure that these values were calculated using SQL
 
+		// clear the database
 		ProgramDatabase.clearDB(this.model);
 
+		// store game round number
 		int game1RoundNumber = 0;
 
-
+		// start a game
 		this.model.startGame();
 
-
+		// run game loop
 		while(this.model.getGameState()==DataGame.GameState.RUNNING) {
 			int category;
 
@@ -308,23 +344,29 @@ public class GameTest{
 
 		}
 
+		// assert that the database returns the correct avg number of draws, because only 1 game has been played and the avg should be
+		// the number of draws in the last game
 		assertEquals(DataGame.getAvgNumberOfDraws(), this.model.getNumberOfDraws(), 0.001);
 
+		// if the game was won by human or AI, assert that the database is returning the correct number of AI or human wins
 		if(this.model.getGameWinner().getType() == DataPlayer.PlayerType.HUMAN) {
 			assertEquals(1, DataGame.getNumberOfHumanWins());
 		} else {
 			assertEquals(1, DataGame.getNumberOfAIWins());
 		}
 
+		// assert that only 1 game is played in database
 		assertEquals(1, DataGame.getNumberOfGames());
 
+		// next game round number
 		int game2RoundNumber = 0;
 
+		// start a new game
 		this.model = new DataGame(4);
-
 		this.model.startGame();
 
 
+		// run game loop
 		while(this.model.getGameState()==DataGame.GameState.RUNNING) {
 			int category;
 
@@ -344,8 +386,10 @@ public class GameTest{
 
 		}
 
+		// if game 1 round number was higher than game 2 round number, game 1 was longer, and vice versa
 		int higherRoundNumber = game2RoundNumber>game1RoundNumber ? game2RoundNumber : game1RoundNumber;
 
+		// check the longer round number is being returned by the database
 		assertEquals(higherRoundNumber, DataGame.getLongestGame());
 
 	}
--- src/test/UnitTest.java
+++ src/test/UnitTest.java
@@ -36,8 +36,10 @@ public class UnitTest {
 		// test best category is selected for active players
 		DataPlayer player = new DataPlayer(DataPlayer.PlayerType.HUMAN, 0);
 
+		// add the first card in the ordered deck to the players deck
 		player.addCardToDeck(DataCardCache.getAllCardsInOrder()[0]);
 
+		// assert that category 2 is returned by the method. We know this because we know the card
 		assertEquals(2, this.model.getBestCategoryForPlayer(player));
 	}
 
@@ -45,6 +47,7 @@ public class UnitTest {
 	public void testCardShuffle() {
 		// test card is shuffled properly
 		this.model.startGame();
+		// assert that the first 4 positions in the deck are not the same, using OR because random can end up having multiple same positions
 		assert(
 				!this.model.getInitialShuffledDeck()[0].equals(this.model.getInitialUnshuffledDeck()[0]) ||
 				!this.model.getInitialShuffledDeck()[1].equals(this.model.getInitialUnshuffledDeck()[1]) ||
@@ -62,26 +65,35 @@ public class UnitTest {
 	@Test
 	public void testListToArrayAndArrayToList() {
 		// test array to list and list to array method works properly
+
+		// start an instance
 		this.model.startGame();
+
+		// create player lists and card lists
 		ArrayList<DataPlayer> playersList = new ArrayList<DataPlayer>();
 		playersList.add(this.model.getAllPlayers()[0]);
 		ArrayList<DataCard> cardsList = new ArrayList<DataCard>();
 		cardsList.add(this.model.getInitialUnshuffledDeck()[0]);
 
+		// create player arrays and cards arrays
 		DataPlayer[] playersArr;
 		DataCard[] cardsArr;
 
+		// convert arrays to list
 		playersArr = DataGame.arrayListToArrayPlayer(playersList);
 		cardsArr = DataGame.arrayListToArrayCard(cardsList);
 
+		// assert first values and equal for both
 		assertEquals(playersList.get(0), playersArr[0]);
 		assertEquals(cardsList.get(0), cardsArr[0]);
 
+		// convert lists to arrays
 		ArrayList<DataPlayer> playersList2 = new ArrayList<DataPlayer>();
 		playersList2 = DataGame.arrayToArrayList(playersArr);
 		ArrayList<DataCard> cardsList2 = new ArrayList<DataCard>();
 		cardsList2 = DataGame.arrayToArrayList(cardsArr);
 
+		// assert first values and equal
 		assertEquals(playersList2.get(0), playersArr[0]);
 		assertEquals(cardsList2.get(0), cardsArr[0]);
 	}
@@ -89,10 +101,13 @@ public class UnitTest {
 	@Test
 	public void testTopCategory() {
 		// test card top category is returned
+
+		// create new cards
 		DataCard card = new DataCard("Test", 1,1,1,1,5);
 		DataCard card2 = new DataCard("Test", 1,1,7,1,1);
 		DataCard card3 = new DataCard("Test", 1,4,4,1,1);
 
+		// assert that the best categories are returned, because we know them
 		assertEquals(5, card.findTopCategory());
 		assertEquals(3, card2.findTopCategory());
 		assertTrue(card3.findTopCategory()==2 || card3.findTopCategory()==3);
